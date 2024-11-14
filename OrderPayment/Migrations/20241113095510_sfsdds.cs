12dce0fc6b3402bcec56649685cf03fc169df145
ZeQ61 <zeki14@icloud.com>
2024-11-15 01:07:38 +0300

    rersim ekle

---
+++ OrderPayment/Migrations/20241113095510_sfsdds.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderPayment.Migrations
+{
+    /// <inheritdoc />
+    public partial class sfsdds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "products");
+        }
+    }
+}
---
+++ OrderPayment/Models/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderPayment.Models
+{
+    public class LoginRequest
+    {
+        [Required]
+        [Phone(ErrorMessage = "Geçersiz telefon numarası.")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifre en az 6 karakter olmalıdır.")]
+        public string Password { get; set; }
+    }
+}
